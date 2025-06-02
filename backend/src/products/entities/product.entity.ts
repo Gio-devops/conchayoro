221f963fca376ac612102dbeedd41b3c7510f894
Gio-devops <giovani.consigliog@gmail.com>
2025-06-02 19:39:34 -0300

    project backend

--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -3,23 +3,23 @@ import { UUIDV4 } from 'sequelize';
 
 @Table({ tableName: 'Product', createdAt: true, updatedAt: true })
 export class Product extends Model {
-    @Column({
-        type: DataType.UUID,
-        primaryKey: true,
-        defaultValue: UUIDV4,
-        allowNull: false,
-    })
-    id: string;
-    
-    @Column({ allowNull: false, unique: true })
-    name: string;
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false,
+  })
+  id: string;
 
-    @Column({})
-    price: number;
+  @Column({ allowNull: false, unique: true })
+  name: string;
 
-    @Column({})
-    category: string;
+  @Column({})
+  price: number;
 
-    @Column({})
-    rating: number;
+  @Column({})
+  category: string;
+
+  @Column({})
+  rating: number;
 }
